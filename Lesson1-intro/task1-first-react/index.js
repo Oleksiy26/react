8d89d85ab4b6dc38e893c9c9fb80fb5666d834cd
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-01-23 18:25:52 +0200

    first react

--- Lesson1-intro/task1-first-react/index.js
+++ Lesson1-intro/task1-first-react/index.js
@@ -1,13 +1,4 @@
-/* global React, ReactDOM */
-/* eslint no-undef: "error" */
-
 const rootElem = document.querySelector('#root');
-
-// const greetingElem = document.createElement('div');
-// greetingElem.classList.add('greeting');
-// greetingElem.textContent = 'Hello, React!';
-
-// rootElem.append(greetingElem);
 
 const greetingElem = React.createElement('div', { className: 'greeting' }, 'Hello, React!');
 
