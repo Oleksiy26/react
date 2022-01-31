268d518b02e7d7e946704a20cd1a21d8a55acd1e
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-01-31 18:47:13 +0200

    goodbutton

---
+++ Lesson5-events/task1-good_button/src/GoodButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const GoodButton = () => (
+  <div className="fancy-button" onClick={() => alert('Good job!')}>
+    Click me!
+  </div>
+);
+export default GoodButton;
--- Lesson5-events/task1-good_button/src/index.jsx
+++ Lesson5-events/task1-good_button/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Counter from './Counter.jsx';
+import GoodButton from './GoodButton.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Counter />, rootElement);
+ReactDOM.render(<GoodButton />, rootElement);
