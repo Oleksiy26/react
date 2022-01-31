b1f0fe6c3b75da546eb3e8170ae909bff3dbab54
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-01-31 18:38:59 +0200

    goodbutton

---
+++ Lesson5-events/task1-good_button/src/GoodButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GoodButton = () => (
+  <div
+    className="fancy-button"
+    onClick={() => {
+      alert('Good job!');
+    }}
+  >
+    Click me!
+  </div>
+);
+export default GoodButton;
