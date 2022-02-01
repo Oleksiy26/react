09ea6102656dfe795b55957a19c92cf54842bd00
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-02-01 03:44:01 +0200

    toggler

--- Lesson5-events/hw1-toggler/src/Toggler.jsx
+++ Lesson5-events/hw1-toggler/src/Toggler.jsx
@@ -5,20 +5,15 @@ class Toggler extends Component {
     super(props);
     this.state = {
       btnText: 'Off',
+      flag: true,
     };
   }
 
-  toggleBtnText = event => {
-    if (event.target.textContent.toLowerCase() === 'off') {
-      this.setState({
-        btnText: 'On',
-      });
-    }
-    if (event.target.textContent.toLowerCase() === 'on') {
-      this.setState({
-        btnText: 'Off',
-      });
-    }
+  toggleBtnText = () => {
+    this.setState({
+      flag: !this.state.flag,
+      btnText: !this.state.flag ? 'On' : 'Off',
+    });
   };
 
   render() {
