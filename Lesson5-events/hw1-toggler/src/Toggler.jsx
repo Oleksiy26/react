f29a82c6ecad87f6b17412a7fd7c17d60cec204f
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-02-01 03:35:56 +0200

    toggler

--- Lesson5-events/hw1-toggler/src/Toggler.jsx
+++ Lesson5-events/hw1-toggler/src/Toggler.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 
 class Toggler extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       btnText: 'Off',
-      flag: true,
     };
   }
 
