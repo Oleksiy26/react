f25f8dcc90f8667f0853bb4b2a8a064da1d79e89
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-02-05 00:04:59 +0200

    transactions

--- Lesson7-lists/hw1-transactions-list/src/Transaction.jsx
+++ Lesson7-lists/hw1-transactions-list/src/Transaction.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import moment from 'moment';
 
-const Transaction = ({ from, to, amount, rate, time }) => (
-  <li className="transaction">
-    <span className="transaction__date">{moment(time).format('DD MMM')}</span>
-    <span className="transaction__time">{moment(time).format('hh:mm')}</span>
-    <span className="transaction__assets">{`${from} → ${to}`}</span>
-    <span className="transaction__rate">{rate}</span>
-    <span className="transaction__amount">{new Intl.NumberFormat('en-GB').format(amount)}</span>
-  </li>
-);
+const Transaction = ({ from, to, amount, rate, time }) => {
+  const dateOfTrans = moment(time).format('DD MMM');
+  const timeOfTrans = moment(time).format('hh:mm');
+  const amountFormated = new Intl.NumberFormat('en-GB').format(amount);
+  return (
+    <li className="transaction">
+      <span className="transaction__date">{dateOfTrans}</span>
+      <span className="transaction__time">{timeOfTrans}</span>
+      <span className="transaction__assets">{`${from} → ${to}`}</span>
+      <span className="transaction__rate">{rate}</span>
+      <span className="transaction__amount">{amountFormated}</span>
+    </li>
+  );
+};
 
 export default Transaction;
