8a4424f3283b9a41bd91d5f1723a4ff6dd82ea6b
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-02-07 03:00:04 +0200

    subscription

--- Lesson8-lifecycle-methods/task3-event-subscription/src/Dimension.jsx
+++ Lesson8-lifecycle-methods/task3-event-subscription/src/Dimension.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Dimensions extends React.Component {
+class Dimension extends React.Component {
   state = {
     width: null,
     height: null,
@@ -34,4 +34,4 @@ class Dimensions extends React.Component {
   }
 }
 
-export default Dimensions;
+export default Dimension;
