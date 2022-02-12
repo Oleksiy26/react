9d13852b17d013d22d58e73d2911d25cbf6633cf
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-02-12 07:26:46 +0200

    expand

--- Lesson11-update-components/hw1-expanded-component/src/Expand.jsx
+++ Lesson11-update-components/hw1-expanded-component/src/Expand.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Expand extends React.Component {
   state = {
     isVisible: false,
-    rotation: 180,
   };
 
-  toggleExpandText = () => {
+  toggleExpandText = e => {
+    const arrowDirection = e.target;
+
+    if (arrowDirection.getAttribute('data-icon') === 'chevron-down') {
+      arrowDirection.setAttribute('data-icon', 'chevron-up');
+    } else {
+      arrowDirection.setAttribute('data-icon', 'chevron-down');
+    }
+
     this.setState({
       isVisible: !this.state.isVisible,
-      rotation: !this.state.isVisible ? 0 : 180,
     });
   };
 
   render() {
-    const { isVisible, rotation } = this.state;
+    const { isVisible } = this.state;
     return (
       <div className="app">
         <div className="expand border">
           <div className="expand__header">
             <span className="expand__title">{this.props.title}</span>
             <button className="expand__toggle-btn" onClick={this.toggleExpandText}>
-              <FontAwesomeIcon icon="fas fa-chevron-up" rotation={rotation} />
+              <i className="fas fa-chevron-down"></i>
             </button>
           </div>
           {isVisible ? (
             <div className="expand__content">
-              {' '}
               <p>
                 Hooks are a new addition in React 16.8. They let you use state and other React
                 features without writing a class.
