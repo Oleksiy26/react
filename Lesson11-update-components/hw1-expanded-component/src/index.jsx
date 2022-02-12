919f1f65eb41efbaca96a247a9a311542e45e92e
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-02-12 04:37:01 +0200

    expand

--- Lesson11-update-components/hw1-expanded-component/src/index.jsx
+++ Lesson11-update-components/hw1-expanded-component/src/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import App from './App.jsx';
+
+library.add(fas);
 
 const rootElement = document.querySelector('#root');
 
