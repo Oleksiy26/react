c6ad91f359752f860b03766e8c22c00e8b7d7f26
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-02-13 03:18:17 +0200

    expand

--- Lesson11-update-components/hw1-expanded-component/src/App.jsx
+++ Lesson11-update-components/hw1-expanded-component/src/App.jsx
@@ -1,33 +1,15 @@
 import React from 'react';
 import Expand from './Expand.jsx';
 
-class App extends React.Component {
-  state = {
-    isVisible: false,
-  };
-
-  toggleExpandText = () => {
-    this.setState({
-      isVisible: !this.state.isVisible,
-    });
-  };
-
-  render() {
-    return (
-      <div className="app">
-        <Expand
-          title="Some title"
-          onExpand={this.toggleExpandText}
-          isVisible={this.state.isVisible}
-        >
-          <p>
-            Hooks are a new addition in React 16.8. They let you use state and other React features
-            without writing a class.
-          </p>
-        </Expand>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="app">
+    <Expand title="Some title">
+      <p>
+        Hooks are a new addition in React 16.8. They let you use state and other React features
+        without writing a class.
+      </p>
+    </Expand>
+  </div>
+);
 
 export default App;
--- Lesson11-update-components/hw1-expanded-component/src/Expand.jsx
+++ Lesson11-update-components/hw1-expanded-component/src/Expand.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Expand = ({ title, children, onExpand, isVisible }) => (
-  <div className="expand border">
-    <div className="expand__header">
-      <span className="expand__title">{title}</span>
-      <button className="expand__toggle-btn" onClick={onExpand}>
-        {isVisible ? (
-          <i className="fas fa-chevron-up"></i>
-        ) : (
-          <i className="fas fa-chevron-down"></i>
-        )}
-      </button>
-    </div>
-    {isVisible ? <div className="expand__content">{children}</div> : null}
-  </div>
-);
+class Expand extends React.Component {
+  state = {
+    isVisible: false,
+  };
+
+  toggleExpandText = () => {
+    this.setState({
+      isVisible: !this.state.isVisible,
+    });
+  };
+
+  render() {
+    const { title, children } = this.props;
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{title}</span>
+          <button className="expand__toggle-btn" onClick={this.toggleExpandText}>
+            {this.state.isVisible ? (
+              <i className="fas fa-chevron-up"></i>
+            ) : (
+              <i className="fas fa-chevron-down"></i>
+            )}
+          </button>
+        </div>
+        {this.state.isVisible ? <div className="expand__content">{children}</div> : null}
+      </div>
+    );
+  }
+}
 
 Expand.propTypes = {
   isVisible: PropTypes.bool,
