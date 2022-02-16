10cd2bf010aef061e49f65a04d53c2b63e1c668e
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-02-17 01:54:30 +0200

    user data

---
+++ Lesson14-react-hooks/task2-users-data/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import User from './User.jsx';
+
+const App = () => (
+  <div className="page">
+    <BrowserRouter>
+      <div className="page__content">
+        <h1>Users</h1>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/users/github">Github</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/users/facebook">Facebook</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/users/:userId">
+            <User />
+          </Route>
+          <Route>
+            <span>Select a user please</span>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </div>
+);
+export default App;
