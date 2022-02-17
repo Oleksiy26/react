eb8d871cb166a2317c914cfff289ddbd148958b6
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-02-17 05:50:37 +0200

    status

--- Lesson14-react-hooks/hw1-event-subscription/src/ConnectionStatus.jsx
+++ Lesson14-react-hooks/hw1-event-subscription/src/ConnectionStatus.jsx
@@ -29,47 +29,4 @@ const ConnectionStatus = () => {
   );
 };
 
-// class ConnectionStatus extends React.Component {
-//   state = {
-//     status: 'online',
-//     flag: true,
-//   };
-
-//   componentDidMount() {
-//     window.addEventListener('online', this.onStatusOnline);
-//     window.addEventListener('offline', this.onStatusOffline);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('online', this.onStatusOnline);
-//     window.removeEventListener('offline', this.onStatusOffline);
-//   }
-
-//   onStatusOffline = () => {
-//     this.setState({
-//       status: 'offline',
-//       flag: false,
-//     });
-//   };
-
-//   onStatusOnline = () => {
-//     this.setState({
-//       status: 'online',
-//       flag: true,
-//     });
-//   };
-
-//   // onStatusChange = () => {
-//   //    //   this.setState({
-//   //     status: navigator.onLine ? 'online' : 'offline',
-//   //   });
-
-//   render() {
-//     if (this.state.flag) {
-//       return <div className="status ">{this.state.status}</div>;
-//     }
-//     return <div className="status status_offline">{this.state.status}</div>;
-//   }
-// }
-
 export default ConnectionStatus;
